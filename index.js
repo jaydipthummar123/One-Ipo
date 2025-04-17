1324c96035dd8ef8d719077ad7f2b0eedc0929cb
jaydipthummar123 <jaydipthummar47@gmail.com>
2025-04-17 15:37:01 +0530

    refactor: reorganize code structure and add object definition

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ let arr = [1,2,3,4,5,6,7,8]
 console.log(arr)
 let jd =0
 console.log(jd)
+  
+let obj = {
+  name: "John",
+  age: 30,
+  occupation: "Developer",
+  hobbies: ["reading", "swimming", "coding"],
+}
